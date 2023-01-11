6b8b98bbf40ee3db6647347d1dc62f9c49901367
DouglasFloro <douglasfloro33@gmail.com>
2023-01-11 00:13:50 -0300

    mapping keyboard keys

--- src/Calculator/index.tsx
+++ src/Calculator/index.tsx
@@ -1,25 +1,153 @@
-import { Divide, Radical } from 'phosphor-react';
+import {
+  Divide,
+  Equals,
+  Minus,
+  NumberEight,
+  NumberFive,
+  NumberFour,
+  NumberNine,
+  NumberOne,
+  NumberSeven,
+  NumberSix,
+  NumberThree,
+  NumberTwo,
+  NumberZero,
+  Percent,
+  Plus,
+  PlusMinus,
+  Radical,
+  X,
+} from 'phosphor-react';
 import * as S from './styles';
 
 export const Calculator: React.FC = () => {
+  const keyboardKey = [
+    {
+      ln: [
+        {
+          id: 0,
+          title: 'AC',
+          value: 'clean',
+        },
+
+        {
+          id: 1,
+          title: <Radical />,
+          value: 'radical',
+        },
+
+        {
+          id: 2,
+          title: <Percent />,
+          value: 'percent',
+        },
+
+        {
+          id: 3,
+          title: <Divide />,
+          value: 'divide',
+        },
+      ],
+    },
+
+    {
+      ln: [
+        {
+          id: 0,
+          title: <NumberSeven />,
+          value: 7,
+        },
+
+        {
+          id: 1,
+          title: <NumberEight />,
+          value: 8,
+        },
+
+        {
+          id: 2,
+          title: <NumberNine />,
+          value: 9,
+        },
+
+        {
+          id: 3,
+          title: <X />,
+          value: 'multiplication',
+        },
+      ],
+    },
+  ];
+
   return (
     <S.Wrapper>
       <S.Container>
-        <S.Result>Resultado</S.Result>
-        <S.Line>
-          <button>AC</button>
-          <button>
-            <Radical />
-          </button>
-          <button>%</button>
-          <button>
-            <Divide />
-          </button>
-        </S.Line>
-        <S.Line></S.Line>
-        <S.Line></S.Line>
-        <S.Line></S.Line>
-        <S.Line></S.Line>
+        <S.Result>0</S.Result>
+        <div>
+          <S.Line>
+            <S.Button>AC</S.Button>
+            <S.Button>
+              <Radical />
+            </S.Button>
+            <S.Button>
+              <Percent />
+            </S.Button>
+            <S.Button>
+              <Divide />
+            </S.Button>
+          </S.Line>
+          <S.Line>
+            <S.Button>
+              <NumberSeven />
+            </S.Button>
+            <S.Button>
+              <NumberEight />
+            </S.Button>
+            <S.Button>
+              <NumberNine />
+            </S.Button>
+            <S.Button>
+              <X />
+            </S.Button>
+          </S.Line>
+          <S.Line>
+            <S.Button>
+              <NumberFour />
+            </S.Button>
+            <S.Button>
+              <NumberFive />
+            </S.Button>
+            <S.Button>
+              <NumberSix />
+            </S.Button>
+            <S.Button>
+              <Minus />
+            </S.Button>
+          </S.Line>
+          <S.Line>
+            <S.Button>
+              <NumberOne />
+            </S.Button>
+            <S.Button>
+              <NumberTwo />
+            </S.Button>
+            <S.Button>
+              <NumberThree />
+            </S.Button>
+            <S.Button>
+              <Plus />
+            </S.Button>
+          </S.Line>
+          <S.Line isGrid>
+            <S.Button variant="lg">
+              <NumberZero />
+            </S.Button>
+            <S.Button>,</S.Button>
+            <S.Button>
+              <Equals />
+            </S.Button>
+          </S.Line>
+        </div>
       </S.Container>
     </S.Wrapper>
   );
--- src/Calculator/styles.ts
+++ src/Calculator/styles.ts
@@ -1,27 +1,62 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const Wrapper = styled.div`
-  width: 100%;
+  width: 100vw;
+  height: 100vh;
 
   display: flex;
   justify-content: center;
+  align-items: center;
 `;
 
 export const Container = styled.div`
-  width: 250px;
-  height: 250px;
+  width: 240px;
 
   display: flex;
   flex-direction: column;
   background: #000;
+
+  & > div {
+    display: flex;
+    flex-direction: column;
+    gap: 6px;
+    padding: 10px;
+  }
 `;
 
-export const Result = styled.div`
+export const Result = styled.section`
   height: 35px;
   background: #fff;
   color: #000;
 `;
-
-export const Line = styled.div`
+export const Line = styled.div<{ isGrid?: boolean }>`
   display: flex;
+  justify-content: space-between;
+  gap: 8px;
+  ${({ isGrid }) =>
+    isGrid &&
+    css`
+      display: grid;
+      grid-template-columns: 2fr 1fr 1fr;
+    `}
+`;
+
+export const Button = styled.button<{ variant?: 'lg' }>`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+
+  background: #333333;
+
+  ${({ variant }) =>
+    variant === 'lg' &&
+    css`
+      width: 100%;
+      border-radius: 50px;
+      display: flex;
+      align-content: center;
+      justify-content: flex-start;
+      align-items: center;
+      padding: 0 16px;
+    `}
 `;
